d645c4b382cc27f9ef21378e0e0a3da6c841043d
lhclopes <luizhen@gmail.com>
2025-03-12 22:55:52 -0300

    Conta Terminal

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /**
  *
  * @author luizhenrique
@@ -5,6 +8,27 @@
 public class ContaTerminal {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Desafio: Projeto Conta Terminal");
+
+        int numero;
+        String agencia;
+        String nomeCliente;
+        Double saldo;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Por Favteor, Digite o nome do Cliente: ");
+        nomeCliente = scanner.nextLine();
+
+        System.out.print("Por Favor, Digite o número da agencia: ");
+        agencia = scanner.nextLine();
+
+        System.out.print("Por Favor, Digite o número da conta: ");
+        numero = scanner.nextInt();
+
+        System.out.print("Por Favor, Digite o Saldo: ");
+        saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua Agência é " + agencia + ", conta " + numero + "e seu saldo " + saldo + " já está disponivel para saque");
     }
 }
